6863b3630f06dc82ec6fd93d89bb004e1f264bb7
foxtrot12 <43786672+foxtrot12@users.noreply.github.com>
2024-07-31 18:51:29 +0000

    first cut

---
+++ photo-port/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-screen w-screen"></div>
+</template>
+
+<style scoped>
+</style>
---
+++ photo-port/src/main.ts
@@ -0,0 +1,5 @@
+import './assets/tailwind.scss'; 
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
